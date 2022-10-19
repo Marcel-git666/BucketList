fcd88aaee00db7dc60105034ef41e0291b02cc11
Marcel Mravec <marcel.mravec@gmail.com>
2022-10-19 16:06:30 +0200

    Biometric authentication added

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import MapKit
+import LocalAuthentication
 import SwiftUI
 
 
@@ -16,6 +17,8 @@ struct Location: Identifiable {
 }
 
 struct ContentView: View {
+    
+    @State private var isUnlocked = false
     
     @State private var mapReagion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
                                            
@@ -36,7 +39,27 @@ struct ContentView: View {
                     }
                 }
             }
-            .navigationTitle("London Explorer")
+            .navigationTitle(isUnlocked ? "L. Explorer unlocked": "L. Eplorer locked")
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    //we have authenticated successfully
+                    isUnlocked = true
+                } else {
+                    //there was a problem
+                }
+            }
+        } else {
+            //no biometrics
         }
     }
     
